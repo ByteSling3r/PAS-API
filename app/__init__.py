8154833b94b4ea554086f43549c26278790447ef
Juan Delgado <juanddelgadoguerra@gmail.com>
2024-06-18 19:01:22 -0500

    endpoint device_state finish

---
+++ app/__init__.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def index():
+    return render_template('templates/index.html')
+
+
+class DeviceState(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    device_name = db.Column(db.String(50), unique=True, nullable=False)
+    state = db.Column(db.String(10), nullable=False)
+
+    def __repr__(self):
+        return f'<DeviceState {self.device_name}: {self.state}>'
+
+
+@app.route('/api/state', methods=['GET'])
+def get_state():
+    device_states = DeviceState.query.all()
+    state_dict = {device.device_name: device.state for device in device_states}
+    return jsonify(state_dict)
+
+
+@app.route('/api/state', methods=['PUT'])
+def set_state():
+    data = request.json
+    for device_name, state in data.items():
+        device = DeviceState.query.filter_by(device_name=device_name).first()
+        if device:
+            device.state = state
+        else:
+            new_device = DeviceState(device_name=device_name, state=state)
+            db.session.add(new_device)
+    db.session.commit()
+    return jsonify({"message": "Device states updated successfully"})
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
