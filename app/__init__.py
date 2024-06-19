ba3fd00b81aacaa28e5f707a32c6dac551479a89
Juan Delgado <juanddelgadoguerra@gmail.com>
2024-06-18 19:02:28 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -6,52 +6,50 @@ import os
 from dotenv import load_dotenv
 
 
-app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+def create_app():
+    app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-load_dotenv()
+    load_dotenv()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-@app.route('/')
-def index():
-    return render_template('templates/index.html')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
 
 
-class DeviceState(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    device_name = db.Column(db.String(50), unique=True, nullable=False)
-    state = db.Column(db.String(10), nullable=False)
-
-    def __repr__(self):
-        return f'<DeviceState {self.device_name}: {self.state}>'
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
 
-@app.route('/api/state', methods=['GET'])
-def get_state():
-    device_states = DeviceState.query.all()
-    state_dict = {device.device_name: device.state for device in device_states}
-    return jsonify(state_dict)
+    class DeviceState(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        device_name = db.Column(db.String(50), unique=True, nullable=False)
+        state = db.Column(db.String(10), nullable=False)
+
+        def __repr__(self):
+            return f'<DeviceState {self.device_name}: {self.state}>'
 
 
-@app.route('/api/state', methods=['PUT'])
-def set_state():
-    data = request.json
-    for device_name, state in data.items():
-        device = DeviceState.query.filter_by(device_name=device_name).first()
-        if device:
-            device.state = state
-        else:
-            new_device = DeviceState(device_name=device_name, state=state)
-            db.session.add(new_device)
-    db.session.commit()
-    return jsonify({"message": "Device states updated successfully"})
+    @app.route('/api/state', methods=['GET'])
+    def get_state():
+        device_states = DeviceState.query.all()
+        state_dict = {device.device_name: device.state for device in device_states}
+        return jsonify(state_dict)
 
 
-if __name__ == '__main__':
+    @app.route('/api/state', methods=['PUT'])
+    def set_state():
+        data = request.json
+        for device_name, state in data.items():
+            device = DeviceState.query.filter_by(device_name=device_name).first()
+            if device:
+                device.state = state
+            else:
+                new_device = DeviceState(device_name=device_name, state=state)
+                db.session.add(new_device)
+        db.session.commit()
+        return jsonify({"message": "Device states updated successfully"})
+    return app
 
-    app.run(host='0.0.0.0', port=5000)
